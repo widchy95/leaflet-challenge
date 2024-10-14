b6e9120e5a8a4b064d51420a428fffcb55d33110
Widchy Joachim <jwidchy95@gmail.com>
2024-10-14 18:29:40 -0400

    logic.js

--- Leaflet-Part-2/static/js/logic.js
+++ Leaflet-Part-2/static/js/logic.js
@@ -4,12 +4,12 @@ var myMap = L.map("map").setView([37.09, -95.71], 5);
 // Base layers for the map
 var streets = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-}).addTo(myMap); // Add streets layer by default
+}); 
 
 var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
   maxZoom: 18
-});
+}).addTo(myMap); // Add satellite layer by default
 
 var grayscale = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
